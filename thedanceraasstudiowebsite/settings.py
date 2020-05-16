a05c35857f6777de36a8fb7a8dde7c50d965845d
deepakdevp <deepakdevp@gmail.com>
2020-05-16 20:40:21 +0530

    Arranged static files and image urls

--- thedanceraasstudiowebsite/settings.py
+++ thedanceraasstudiowebsite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'thedanceraasstudiowebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,8 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'thedanceraasstudiowebsite.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
